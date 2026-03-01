6f8e4e14e9d97c3ed5432f3c02a156cff5067b26
William Horn <wbhorn@alaska.edu>
2026-02-28 21:50:39 -0900

    Update geos lab3

--- geos639/labs/lab3/plot-dems.py
+++ geos639/labs/lab3/plot-dems.py
@@ -3,43 +3,31 @@ import rasterio
 import numpy as np
 
 
-def plot_dems():
-    lab, homework = 'lab-dem.tif', 'homework-dem.tif'
+lab, homework = 'lab-dem.tif', 'homework-dem.tif'
 
-    with rasterio.open(lab) as ds:
-        lab_dem = ds.read(1)
+with rasterio.open(lab) as ds:
+    lab_dem = ds.read(1)
 
-    with rasterio.open(homework) as ds:
-        hw_dem = ds.read(1)
+with rasterio.open(homework) as ds:
+    hw_dem = ds.read(1)
 
-    lab_dem[lab_dem == -32767] = np.nan
-    hw_dem[hw_dem == -32767] = np.nan
+lab_dem[lab_dem == -32767] = np.nan
+hw_dem[hw_dem == -32767] = np.nan
 
-    fig, axs = plt.subplots(1, 3, figsize=(12, 5))
+fig, axs = plt.subplots(1, 2, figsize=(12, 5))
 
-    axs[0].imshow(lab_dem, cmap='gray')
-    axs[0].set_title('Lab DEM')
-    axs[0].axis('off')
+min_pct, max_pct = 0.25, 99.75
+lab_vmin, lab_vmax = np.nanpercentile(lab_dem, min_pct), np.nanpercentile(lab_dem, max_pct)
+hw_vmin, hw_vmax = np.nanpercentile(hw_dem, min_pct), np.nanpercentile(hw_dem, max_pct)
 
-    axs[1].imshow(hw_dem, cmap='gray')
-    axs[1].set_title('Homework DEM')
-    axs[1].axis('off')
+lab_ax = axs[0].imshow(lab_dem, cmap='gray', vmin=lab_vmin, vmax=lab_vmax)
+axs[0].set_title('Lab DEM')
+axs[0].axis('off')
+fig.colorbar(lab_ax, ax=axs[0], label='Elevation (m)', orientation='vertical', shrink=0.8)
 
-    min_h = min(lab_dem.shape[0], hw_dem.shape[0])
-    min_w = min(lab_dem.shape[1], hw_dem.shape[1])
+hw_ax = axs[1].imshow(hw_dem, cmap='gray', vmin=hw_vmin, vmax=hw_vmax)
+axs[1].set_title('Homework DEM')
+axs[1].axis('off')
+fig.colorbar(hw_ax, ax=axs[1], label='Elevation (m)', orientation='vertical', shrink=0.8)
 
-    lab_dem = lab_dem[:min_h, :min_w]
-    hw_dem = hw_dem[:min_h, :min_w]
-
-    diff = lab_dem - hw_dem
-    axs[2].imshow(diff, cmap='gray')
-    axs[2].set_title('Difference (lab - hw)')
-    axs[2].axis('off')
-
-    print(f'mean: {np.nanmean(diff)}, std: {np.nanstd(diff)}')
-
-    plt.show()
-
-
-if __name__ == '__main__':
-    plot_dems()
+plt.show()
